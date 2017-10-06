ae6f954744e064367723bf57885ee22188eac967
mvlombardo <mvlombardo@gmail.com>
2017-10-06 19:41:50 +0300

    edits
    a bunch of edits to make this work.

--- plot_fd_dvars_looper.sh
+++ plot_fd_dvars_looper.sh
@@ -16,22 +16,39 @@ do
     subpath=$rootpath/$subid
     preprocpath=$subpath/session_1/preproc_1
     plotspath=$preprocpath/PLOTS
+    
+    # make $plotspath
     mkdir $plotspath
-    cd $preprocpath
-    cp rest_motion_fd.txt $plotspath >> $plotspath/plot_motion.sh
-    cp spp.rest/rest_sm_dvars.txt $plotspath >> $plotspath/plot_motion.sh
-    cp spp.rest/rest_noise_dvars.txt $plotspath >> $plotspath/plot_motion.sh
-    cp spp.rest/rest_wds_dvars.txt $plotspath >> $plotspath/plot_motion.sh
-    cd $plotspath >> $plotspath/plot_motion.sh
+    
+    # write commands into bash script
+    
+    # change directory to $preprocpath
+    echo cd $preprocpath >> $plotspath/plot_motion.sh
+    
+    # copy paste fd and dvars txt files into $plotspath
+    echo cp rest_motion_fd.txt $plotspath >> $plotspath/plot_motion.sh
+    echo cp spp.rest/rest_sm_dvars.txt $plotspath >> $plotspath/plot_motion.sh
+    echo cp spp.rest/rest_noise_dvars.txt $plotspath >> $plotspath/plot_motion.sh
+    echo cp spp.rest/rest_wds_dvars.txt $plotspath >> $plotspath/plot_motion.sh
+    
+    # cd into $plotspath
+    echo cd $plotspath >> $plotspath/plot_motion.sh
 
     # format plot_fd_dvars.py's options
     
+    # format arguments for plot_fd_dvars.py
     FD=rest_motion_fd.txt
     DVARSSM=rest_sm_dvars.txt
     DVARSNOISE=rest_noise_dvars.txt
     DVARSWDS=rest_wds_dvars.txt
     plotpyoptions="--fd $FD --dvars_sm $DVARSSM --dvars_noise $DVARSNOISE --dvars_wds $DVARSWDS"
-    python ~/fmri_spt/plot_fd_dvars.py $plotpyoptions --pdf2save motion_plot.pdf >> $plotspath/plot_motion.sh
-    bash plot_motion.sh
+    
+    # run plot_fd_dvars.py
+    echo python ~/fmri_spt/plot_fd_dvars.py $plotpyoptions --pdf2save motion_plot.pdf >> $plotspath/plot_motion.sh
+    
+    # run bash script
+    bash $plotspath/plot_motion.sh
+    
+    # cd back to $rootpath
     cd $rootpath
 done
